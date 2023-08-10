2b25c0ff1118178676fc2971dd4a5264765e71cd
fabio-souza <101989744+fabio-leitesouza@users.noreply.github.com>
2023-08-10 17:30:11 -0300

    update

--- Assets/Script/ControlaZumbi.cs
+++ Assets/Script/ControlaZumbi.cs
@@ -5,15 +5,28 @@ using UnityEngine;
 public class ControlaZumbi : MonoBehaviour
 {
     public GameObject Player;
-    // Start is called before the first frame update
-    void Start()
+    public float Velocidade = 5;
+    
+    void FixedUpdate()
     {
-        
-    }
+        float distancia = Vector3.Distance(transform.position, Player.transform.position);
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Vector3 direcaoJogador = Player.transform.position - transform.position;
+        Quaternion novaRotacao = Quaternion.LookRotation(direcaoJogador);
+        GetComponent<Rigidbody>().MoveRotation(novaRotacao);
+
+        if(distancia > 0.2)
+        {
+             Vector3 direcao = Player.transform.position - transform.position;
+            GetComponent<Rigidbody>().MovePosition
+            (GetComponent<Rigidbody>().position + 
+            (direcao.normalized * Velocidade * Time.deltaTime));
+            GetComponent<Animator>().SetBool("Ataque", false); 
+        }
+        else
+        {
+            GetComponent<Animator>().SetBool("Ataque", true);
+        }
+       
     }
 }
