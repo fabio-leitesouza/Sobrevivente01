eb3083d9b20b042c030158ddc18acd0516767bb9
fabio-souza <101989744+fabio-leitesouza@users.noreply.github.com>
2023-08-10 15:24:01 -0300

    update

---
+++ Assets/Script/PlayerBehaviourScript.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehaviourScript : MonoBehaviour
+{
+    public float Velocidade = 8;
+    Vector3 direcao;
+
+    void Update()
+    {
+        float eixoX = Input.GetAxis("Horizontal");
+        float eixoZ = Input.GetAxis("Vertical");
+
+        direcao = new Vector3(eixoX, 0, eixoZ);
+
+        if(direcao != Vector3.zero){
+            GetComponent<Animator>().SetBool("Correndo", true);
+        }
+        else
+        {
+            GetComponent<Animator>().SetBool("Correndo", false);
+        }        
+
+    }
+
+    void FixedUpdate()
+    {
+        GetComponent<Rigidbody>().MovePosition
+            (GetComponent<Rigidbody>().position + 
+            (direcao * Time.deltaTime * Velocidade));
+    }
+}
