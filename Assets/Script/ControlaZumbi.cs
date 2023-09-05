3c90d2968f92bb6c8435e377356de36ebabebc08
fabio-souza <101989744+fabio-leitesouza@users.noreply.github.com>
2023-09-05 07:32:21 -0300

    Update ControlaZumbi.cs

--- Assets/Script/ControlaZumbi.cs
+++ Assets/Script/ControlaZumbi.cs
@@ -6,7 +6,11 @@ public class ControlaZumbi : MonoBehaviour
 {
     public GameObject Player;
     public float Velocidade = 5;
-    
+    private Vector3 posicaoAleatoria; // para o zumbi andar aleatoriamente
+    private Vector3 direcao; // para o zumbi andar aleatoriamente
+    private float contadorVagar; // para o zumbi andar aleatoriamente
+    private float tempoNovaPosicaoAleatoria = 4; // para o zumbi andar aleatoriamente
+
     void Start()
     {
         Player = GameObject.FindWithTag("Player");
@@ -15,28 +19,69 @@ public class ControlaZumbi : MonoBehaviour
     void FixedUpdate()
     {
         float distancia = Vector3.Distance(transform.position, Player.transform.position);
+        
 
-        Vector3 direcaoJogador = Player.transform.position - transform.position;
-        Quaternion novaRotacao = Quaternion.LookRotation(direcaoJogador);
-        GetComponent<Rigidbody>().MoveRotation(novaRotacao);
-
-        if(distancia > 3.5)
+        if (distancia > 15)
         {
-            Vector3 direcao = Player.transform.position - transform.position;
-            GetComponent<Rigidbody>().MovePosition
-            (GetComponent<Rigidbody>().position + 
-            (direcao.normalized * Velocidade * Time.deltaTime));
-            GetComponent<Animator>().SetBool("Ataque", false); 
+            Vagar();
         }
         else
         {
-            GetComponent<Animator>().SetBool("Ataque", true);
-        }       
+            direcao = Player.transform.position - transform.position; // direcao para onde o zumbi vai andar
+
+            Quaternion novaRotacao = Quaternion.LookRotation(direcao);
+            GetComponent<Rigidbody>().MoveRotation(novaRotacao);
+
+            Vector3 direcaoMovimento = Player.transform.position - transform.position;
+            GetComponent<Rigidbody>().MovePosition(GetComponent<Rigidbody>().position +
+                (direcaoMovimento.normalized * Velocidade * Time.deltaTime));
+
+            if (distancia > 3.5)
+            {
+                GetComponent<Animator>().SetBool("Ataque", false);
+            }
+            else
+            {
+                GetComponent<Animator>().SetBool("Ataque", true);
+            }
+        }
+        void Vagar()
+        {
+            contadorVagar -= Time.deltaTime;
+            if (contadorVagar <= 0)
+            {
+                posicaoAleatoria = AleatoriarDirecao();
+                contadorVagar += tempoNovaPosicaoAleatoria;
+            }
+
+            bool ficouPertoOSuficiente = Vector3.Distance(transform.position, posicaoAleatoria) <= 0.05;
+            if (ficouPertoOSuficiente == false)
+            {
+                direcao = posicaoAleatoria - transform.position; // direcao para onde o zumbi vai andar
+                GetComponent<Rigidbody>().MovePosition
+                (GetComponent<Rigidbody>().position +
+                (direcao.normalized * Velocidade * Time.deltaTime));
+                GetComponent<Animator>().SetBool("Ataque", false);
+            }
+            else
+            {
+                contadorVagar = 0;
+            }
+
+        }
+        Vector3 AleatoriarDirecao()
+        {
+            Vector3 posicao = Random.insideUnitSphere * 20; // dentro de uma esfera de raio 20
+            posicao += transform.position; // para nao ficar dentro do zumbi
+            posicao.y = transform.position.y; //mantem a altura
+
+            return posicao; // retorna a posicao aleatoria
+        }
     }
     void AtacaJogador()
     {
         int dano = Random.Range(20, 30);
         ControlaJogador playerScript = Player.GetComponent<ControlaJogador>();
-        playerScript.TomarDano(dano);        
+        playerScript.TomarDano(dano);
     }
-}   
+}
